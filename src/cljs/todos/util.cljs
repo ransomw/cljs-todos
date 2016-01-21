9582c7399ee21b3108eb3fffe1b457b34d052852
ransom <auvergnerw@gmail.com>
2016-01-21 12:08:15 -0800

    update date-display-str

--- src/cljs/todos/util.cljs
+++ src/cljs/todos/util.cljs
@@ -1,5 +1,6 @@
 (ns todos.util
   (:require
+   [clojure.string :as str]
    [cljs-time.core :as t]
    [cljs-time.format :as tf]
    [cljs-time.coerce :as tc]
@@ -8,16 +9,13 @@
   )
 
 (def date-input-formatter (tf/formatters :year-month-day))
-(def date-display-formatter
-   (tf/formatters :year-month-day))
 
 (defn date-str? [str]
   (try
     (if (tf/parse date-input-formatter str)
       true)
     (catch :default e
-      false))
-  )
+      false)))
 
 (defn date-str-leq [date-str-lt date-str-gt]
   (let [parse-date
@@ -28,22 +26,20 @@
     (t/before? date-lt date-gt)))
 
 (defn date-display-str [date-str]
-
-  date-str
-
-  ;;;;; todo: pretty printing
-  ;;;;; __ days ago, yesterday, today, tomorrow, Monday, October 30th...
-
-  ;; (let [date (tc/to-local-date (tf/parse date-input-formatter date-str))]
-  ;;   (js/console.log date)
-
-  ;;   (cond
-  ;;     (t/= (t/today) date)
-  ;;     "today"
-  ;;     :else
-  ;;     (tf/unparse (tf/formatters :year-month-day)
-  ;;                 (tc/to-date-time date)
-  ;;     )
-  ;;   )
-
-  )
+  (if date-str
+    (let [date-obj (tc/to-local-date
+                    (tf/parse date-input-formatter date-str))
+          today (tc/to-local-date (t/today))]
+      (cond
+        (t/before? date-obj today)
+        "overdue!"
+        (t/= date-obj today)
+        "today"
+        (> 7 (t/in-days (t/interval today date-obj)))
+        (get (str/split (tf/unparse (tf/formatters :rfc822)
+                                    (tc/to-date-time date-obj))
+                        ",") 0)
+        :else
+        (tf/unparse (tf/formatters :year-month-day)
+                    (tc/to-date-time date-obj))
+        ))))
